5c7b5942c84b1ed94aeaf3d3cc79481fcec9d514
TonnyMusembi <musembutonnyj@gmail.com>
2022-12-05 15:05:50 +0300

    add

--- components/TheRegister.vue
+++ components/TheRegister.vue
@@ -1,4 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const test = ref();
+const register = async () => {
+  console.log(test);
+};
+</script>
 <template>
-  <div class="container mt-auto"></div>
+  <div class="container fjustify-center items-center h-screen">
+    <form @submit.prevent="register">
+      <div class="mb-6">
+        <label for="phone">phone number</label>
+      </div>
+    </form>
+  </div>
 </template>
